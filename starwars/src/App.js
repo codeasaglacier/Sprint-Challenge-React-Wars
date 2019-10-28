a196f856c06c6eb6bd0d77d2c3645f927c05cd7a
codeasaglacier <remingtonshock@gmail.com>
2019-10-27 18:02:10 -0800

    still have errors, out of time

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +10,46 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://swapi.co/api/people/1/') 
+      .then(response => {
+        console.log(response)
+        setPeople(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      }
+      )
+    }, [])
+
+const SWCard = props => {
+  return (
+    <div>
+      <h2>Character: {props.name}</h2>
+      <p>Hair Color: {props.hair_color}</p>
+    </div>
+  )
+}
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {people.map((person, i) => {
+        return(
+          <div>
+            <SWCard
+              key = {person.i}
+              character = {person.name}
+              hair = {person.hair_color}
+              />
+          </div>
+        )
+      })}
     </div>
   );
 }
 
-export default App;
+export default App
